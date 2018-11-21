8dbb14202e125dc693337560771823282435011d
Infant Frontender <newbornfrontender@gmail.com>
2018-11-21 20:50:18 +0300

    Completed lessons: 5-6

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log('Hello TypeScript!');
+const pizzas = [
+  {
+    name: 'Pepperoni',
+    topings: ['pepperoni'],
+  },
+];
+
+const mappedPizzas = pizzas.map((pizza, index) => pizza.name.toUpperCase());
+
+console.log(mappedPizzas);
+
+const pizza = {
+  name: 'Blazing Inferno',
+  getName() {
+    setTimeout(() => console.log(this.name), 1000);
+  },
+};
+
+pizza.getName();
