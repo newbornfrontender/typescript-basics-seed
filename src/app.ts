24d8551cf1292531faa751104f1d5b5b2689fadd
Infant Frontender <newbornfrontender@gmail.com>
2018-11-22 21:27:25 +0300

    Complete 19 lesson

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
-function orderError(error: string): never {
-  throw new Error(error);
+let coupon: string | null = 'pizza25';
+
+function removeCoupon(): void {
+  coupon = null;
 }
 
-orderError('Something went wrong!');
+console.log(coupon);
+
+removeCoupon();
+
+console.log(coupon);
